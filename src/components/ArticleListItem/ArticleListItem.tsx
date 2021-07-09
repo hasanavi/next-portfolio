2d61906417eefbca1b87e2c4bc8fd4cc15b9cbe3
Hasan Haque <hasanavi@gmail.com>
2021-07-09 01:51:07 +0100

    Article List item style

--- src/components/ArticleListItem/ArticleListItem.tsx
+++ src/components/ArticleListItem/ArticleListItem.tsx
@@ -7,66 +7,44 @@ interface articleListItemProps {
   publishDate?: string;
   info1: string;
   info2?: string;
-  img: string;
+  img?: string;
   title: string;
 }
 
-const ArticleListItem: FC<articleListItemProps> = (props) => {
-  let publishDate;
-  let info2;
+const ArticleListItem: FC<articleListItemProps> = ({
+  publishDate,
+  info1,
+  info2,
+  img,
+  title,
+  children,
+}) => {
   let imgColumn;
-  let articleListItemClass =
-    "grid-x grid-margin-x grid-padding-y article-list-item";
+  let articleListItemClass = "article-list-item";
   let additionalInfo;
 
-  if (props.publishDate) {
-    publishDate = (
-      <time className="h5 article-list-item__time">{props.publishDate}</time>
-    );
-  }
-
-  if (props.info2) {
-    info2 = (
-      <>
-        <span className="article-list-item__info-separator">/</span>
-        <p className="article-list-item__info2">{props.info2}</p>
-      </>
-    );
-  }
-
-  if (props.img) {
-    imgColumn = (
-      <div className="cell small-12 medium-6 article-list-item__image-container">
-        <Image
-          src={props.img}
-          alt={props.title}
-          width={420}
-          height={233}
-          priority
-        />
-      </div>
-    );
-
-    articleListItemClass += " article-list-item--with-image";
-  }
-
-  if (props.children) {
-    additionalInfo = (
-      <p className="article-list-item__additional-info">{props.children}</p>
-    );
-  }
-
   return (
-    <div className={articleListItemClass}>
-      <div className="cell small-12 medium-auto article-list-item__info-container">
-        {publishDate}
-        <h2 className="h3 article-list-item__heading">{props.title}</h2>
-        <span className="article-list-item__info-separator">/</span>
-        <p className="article-list-item__info1">{props.info1}</p>
-        {info2}
-        {additionalInfo}
+    <div className={styles.articleListItem}>
+      <div className={styles.infoContainer}>
+        {publishDate ? (
+          <time className={["h5", styles.time].join(" ")}>{publishDate}</time>
+        ) : null}
+        <h2 className={["h3", styles.heading].join(" ")}>{title}</h2>
+        <span className={styles.infoSeparator}>/</span>
+        <p className={styles.info1}>{info1}</p>
+        {info2 ? (
+          <>
+            <span className={styles.infoSeparator}>/</span>
+            <p className={styles.info2}>{info2}</p>
+          </>
+        ) : null}
+        {children ? <p className={styles.additionalInfo}>{children}</p> : null}
       </div>
-      {imgColumn}
+      {img ? (
+        <div className={styles.imageContainer}>
+          <Image src={img} alt={title} width={420} height={233} priority />
+        </div>
+      ) : null}
     </div>
   );
 };
