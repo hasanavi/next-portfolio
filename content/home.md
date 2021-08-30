5a8b6bb92348eb2cffa52b4255c259c227e6cf53
Hasan Haque <hasanavi@gmail.com>
2021-08-30 02:18:01 +0100

    Home page CMS integration

--- content/home.md
+++ content/home.md
@@ -1,6 +1,6 @@
 ---
 page_title: ABOUT ME
-page_subtitle: Hello, My name is Hasan Azizul Haque
+page_subtitle: Hello, My name is Hasan Haque
 page_description: I'm a London based Javascript Developer. I have over 14 years
   experience (5 years full stack + 9 years front-end) developing web and mobile
   application. I love building unique, efficient site and passionate about
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,37 +3,29 @@ import Link from "next/link";
 import Intro from "../components/Intro/Intro";
 import KeepInTouch from "../components/KeepInTouch/KeepInTouch";
 import styles from "../styles/Home.module.css";
+import { attributes } from "../../content/home.md";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <h1>About Me</h1>
-      <Intro className="long">
-        <span className={styles.introText}>
-          Hello, My name is Hasan Azizul Haque
-        </span>
-        <span className={styles.introCopy}>
-          I&apos;m a London based Javascript Developer. I have over 14 years
-          experience (5 years full stack + 9 years front-end) developing web and
-          mobile application. I love building unique, efficient site and
-          passionate about writing clean code.
-        </span>
-      </Intro>
-      <KeepInTouch />
-      <h2 className="text-center">
-        I&apos;ve been lucky to work on these projects:
-      </h2>
-      <Link href="/work">
-        <a>
-          <img
-            className={styles.img}
-            src="/images/clients.png"
-            alt="All clients logo"
-          />
-        </a>
-      </Link>
-    </>
-  );
+	return (
+		<>
+			<h1>{attributes.page_title}</h1>
+			<Intro className="long">
+				<span className={styles.introText}>{attributes.page_subtitle}</span>
+				<span className={styles.introCopy}>{attributes.page_description}</span>
+			</Intro>
+			<KeepInTouch />
+			<h2 className="text-center">{attributes.projects_title}</h2>
+			<Link href="/work">
+				<a>
+					<img
+						className={styles.img}
+						src={attributes.projects_image}
+						alt="All clients logo"
+					/>
+				</a>
+			</Link>
+		</>
+	);
 };
 
 export default Home;
