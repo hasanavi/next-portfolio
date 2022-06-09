9b04bc0ab4c2ead75e08d464e9f8fbf0716caad2
Hasan Haque <hasanavi@gmail.com>
2022-06-09 14:48:39 +0100

    Name Change

--- src/hoc/Layout.tsx
+++ src/hoc/Layout.tsx
@@ -6,34 +6,34 @@ import Navigation from "../components/Navigation/Navigation";
 import TwitterTimeline from "../components/TwitterTimeline/TwitterTimeline";
 import styles from "./Layout.module.css";
 
-const siteTitle = "Hasan Azizul Haque - JavaScript Enthusiast London, UK";
+const siteTitle = "Hasan Haque - JavaScript Enthusiast London, UK";
 
 const Layout: FC = ({ children }) => {
-  return (
-    <>
-      <Head>
-        <title>{siteTitle}</title>
-        <meta
-          name="description"
-          content="I'm a London based Javascript Developer.  I love building unique, efficient site and passionate about writing clean code."
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className={styles.container}>
-        <aside className={styles.aside}>
-          <ProfilePicture />
-          <Intro className="short">
-            I am a<span className={styles.secondaryColor}>Javascript</span>{" "}
-            <span className={styles.tertiaryColor}>Enthusiast</span>
-          </Intro>
-          <Navigation />
-          <TwitterTimeline userName="hasanavi" />
-        </aside>
+	return (
+		<>
+			<Head>
+				<title>{siteTitle}</title>
+				<meta
+					name="description"
+					content="I'm a London based Javascript Developer.  I love building unique, efficient site and passionate about writing clean code."
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className={styles.container}>
+				<aside className={styles.aside}>
+					<ProfilePicture />
+					<Intro className="short">
+						I am a<span className={styles.secondaryColor}>Javascript</span>{" "}
+						<span className={styles.tertiaryColor}>Enthusiast</span>
+					</Intro>
+					<Navigation />
+					<TwitterTimeline userName="hasanavi" />
+				</aside>
 
-        <main className={styles.main}>{children}</main>
-      </div>
-    </>
-  );
+				<main className={styles.main}>{children}</main>
+			</div>
+		</>
+	);
 };
 
 export default Layout;
